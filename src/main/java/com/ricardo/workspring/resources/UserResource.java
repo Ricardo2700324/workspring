58162d9d9cf93de281bb7464bb34bdaf0611c089
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2020-03-24 14:10:31 -0300

    Testando classe de domain USER

--- src/main/java/com/ricardo/workspring/resources/UserResource.java
+++ src/main/java/com/ricardo/workspring/resources/UserResource.java
@@ -1,16 +1,28 @@
 package com.ricardo.workspring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ricardo.workspring.domain.User;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List<User> listar() {
 		
-		return "Rest funcionando";
+		User user1 = new User(1, "Ricardo", "ricardo@gmail.com");
+		User user2 = new User(2, "Tamiris", "Tamiris@gmail.com");
+		
+		List<User> lista = new ArrayList<>();
+		lista.add(user1);
+		lista.add(user2);
+				
+		return lista;
 	}
 }
